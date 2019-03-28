97acf87d6b4847e19b752f3d80320fc711e45896
aslikarahan <aslikarahan96@gmail.com>
2019-03-28 23:36:50 +0300

    !! bug solved

--- shelldon.c
+++ shelldon.c
@@ -1,10 +1,7 @@
 /*
 * shelldon interface program
-
 KUSIS ID: 53940 PARTNER NAME: Asli Karahan
 KUSIS ID: 54040 PARTNER NAME: Gökalp Ünsal
-
-
 */
 
 #include <stdio.h>
@@ -16,8 +13,6 @@ KUSIS ID: 54040 PARTNER NAME: Gökalp Ünsal
 #include <fcntl.h>
 
 #define MAX_LINE       80 /* 80 chars per line, per command, should be enough. */
-
-
 
 int parseCommand(char inputBuffer[], char *args[],int *background, char* file[], int* redir , int* comm_count, char *hist[], int* which_comm);
 int executeCommand(char *args[], char* file[],int redr, int backg, char* hist[], int* comm_count, int which_comm); //char** hist);
@@ -52,7 +47,6 @@ int main(void)
       (3) if command included &, parent will invoke wait()
       */
       executeCommand(args, file, redir, background, history, &command_count, which_command);
-      printf("%d\n", command_count );
     }
   }
   return 0;
@@ -72,34 +66,27 @@ int parseCommand(char inputBuffer[], char *args[],int *background, char* file[],
   start,		/* index where beginning of next command parameter is */
   ct,	        /* index of where to place the next parameter into args[] */
   command_number;	/* index of requested command number */
-
   ct = 0;
   int ct_2 =0;
   /* read what the user enters on the command line */
-//  printf("Given input buffer is %s\n", inputBuffer);
+  *comm_count = *comm_count+1; //increment the command count first
+  int asd = *comm_count;
 
-*comm_count = *comm_count+1; //increment the command count first
-int asd = *comm_count;
-
-if(*redir!=4 && *redir !=5){
+  if(*redir!=4 && *redir !=5){
     memset(inputBuffer, 0, MAX_LINE * sizeof(char));
-  do {
-    printf("shelldon>");
-    fflush(stdout);
-    length = read(STDIN_FILENO,inputBuffer,MAX_LINE);
+    do {
+      printf("shelldon>");
+      fflush(stdout);
+      length = read(STDIN_FILENO,inputBuffer,MAX_LINE);
+    }
+    while (inputBuffer[0] == '\n'); /* swallow newline characters */
+
+  }else{
+    length=strlen(inputBuffer);
   }
-  while (inputBuffer[0] == '\n'); /* swallow newline characters */
-  //printf(" Current input buffer is %s\n", inputBuffer);
 
-}else{
-  length=strlen(inputBuffer);
-}
-
-addToHistory(asd, hist, inputBuffer);
-if(*redir==4) printf("i am here\n" );
-
-
-  // printf("This is pointer val %d\n", asd );
+  addToHistory(asd, hist, inputBuffer);
+  if(*redir==4) printf("i am here\n" );
 
   /**
   *  0 is the system predefined file descriptor for stdin (standard input),
@@ -183,11 +170,9 @@ if(*redir==4) printf("i am here\n" );
         if(inputBuffer[i+1] == '!'){
           *redir=4;
           i=i+1;
-          printf("%s\n",hist[1] );
+          if(*comm_count!=1)
           addToHistory(asd, hist, hist[1]);
-            *comm_count = *comm_count+1;
-          //parseCommand(hist[1], args,background, file, redir, comm_count,hist);
-        //  char*  a = strcat("\n", hist[1]);
+          *comm_count = *comm_count+1;
 
         }else{
           *redir=5;
@@ -201,12 +186,8 @@ if(*redir==4) printf("i am here\n" );
             i=i+1;
           }
           int number = atoi(num);
-          // printf("%d\n", number );
           memset(num, 0, 4 * sizeof(char));
           *which_comm=number;
-          if(hist[*comm_count-*which_comm])
-          addToHistory(asd, hist, hist[*comm_count-*which_comm]);
-          *comm_count = *comm_count+1;
 
         }
       }else if(strncmp(inputBuffer, "history", 7) == 0){
@@ -227,9 +208,6 @@ if(*redir==4) printf("i am here\n" );
   args[--ct] = NULL;
 
   args[ct] = NULL; /* just in case the input line was > 80 */
-
-  // printf("comm_count is %d\n", (*comm_count)%10);
-
   return 1;
 
 } /* end of parseCommand routine */
@@ -245,99 +223,78 @@ int executeCommand(char *args[], char* file[],int redr, int backg, char *hist[],
   if (pid == 0){ //child process
 
 
-    //if(redr==3){ //redr 3: prints the history
     int ct = *comm_count; // command counter
     if(redr==4){ //redr 4: executes the last command on the history
       if(*comm_count != 1){
-      printf("Got so far\n" );
-      parseCommand(hist[0], args, &backg, file, &redr, comm_count, hist, &which_comm);
-    }else{
-  printf("No history yet\n" );}
-    }
-    else if (redr == 5){
-      if(which_comm>=*comm_count){
-      *comm_count=ct+1;
-      printf("You haven't provided that many commands\n");
-    }  else if(which_comm<*comm_count-9){
-      *comm_count=ct+1;
-      printf("Command number not in recent history\n");
-    }else{
-      printf("Executing Command: %s from history\n", hist[*comm_count-which_comm]);
-      *comm_count=ct+1;
-      addToHistory(*comm_count+1, hist, hist[*comm_count-which_comm]);
-      parseCommand(hist[*comm_count-which_comm], args, &backg, file, &redr, comm_count, hist, &which_comm);
-    }
-    }
+        printf("Got so far\n" );
+        parseCommand(hist[0], args, &backg, file, &redr, comm_count, hist, &which_comm);
+      }else{
+        printf("No history yet\n" );}
+      }
+      else if (redr == 5){
+        if(which_comm>=*comm_count){
+          *comm_count=ct+1;
+          printf("You haven't provided that many commands\n");
+        }  else if(which_comm<*comm_count-9){
+          *comm_count=ct+1;
+          printf("Command number not in recent history\n");
+        }else{
+          printf("Executing Command: %s from history\n", hist[*comm_count-which_comm]);
+          parseCommand(hist[*comm_count-which_comm], args, &backg, file, &redr, comm_count, hist, &which_comm);
+        }
+      }
 
-    if(redr ==3){
-      int iter = ct;
-    if(ct>10) iter=10;
+      if(redr ==3){
+        int iter = ct;
+        if(ct>10) iter=10;
         for(int i=1;i<=iter;i++){
           printf("%d- %s", ct-i, hist[i]);
-       }
-     }
-    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-    if(redr ==2){
-      xf=open(file[1],O_RDWR|O_TRUNC|O_CREAT, mode);
-    }else if (redr ==1){
-      xf=open(file[1], O_RDWR|O_APPEND|O_CREAT, mode);
-    }
-    dup2(xf,1);
-    char* path = malloc(strlen("/bin/")+strlen(args[0]));
-    strcpy(path,"/bin/" );
-    strcat(path, args[0] );
-    out=execv(path, args);
-    if (out<0){
-      execvp(args[0], args);
-    }
+        }
+      }
+      mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+      if(redr ==2){
+        xf=open(file[1],O_RDWR|O_TRUNC|O_CREAT, mode);
+      }else if (redr ==1){
+        xf=open(file[1], O_RDWR|O_APPEND|O_CREAT, mode);
+      }
+      dup2(xf,1);
+      char* path = malloc(strlen("/bin/")+strlen(args[0]));
+      strcpy(path,"/bin/" );
+      strcat(path, args[0] );
+      out=execv(path, args);
+      if (out<0){
+        execvp(args[0], args);
+      }
 
 
-    close(xf);
-    exit(1);
+      close(xf);
+      exit(1);
 
 
-  }else if(pid>0){
-  //  if (strncmp(args[0], "cd", 2) == 0){
-  //    chdir(args[1]);
-  //  }
+    }else if(pid>0){
+      //  if (strncmp(args[0], "cd", 2) == 0){
+      //    chdir(args[1]);
+      //  }
+      if(backg != 1){
+        wait(NULL);
 
-
-
-    if(backg != 1){
-      // printf("Waitin for child to exit\n" );
-      wait(NULL);
+      }
 
     }
-    // printf("Child exited\n" );
 
+    return 1;
   }
 
-  // strncpy(hist[*comm_count], *args, MAX_LINE );
-  // if(*comm_count==0){
-  //   hist[0]=args[0];
-  //
-  // }else if (*comm_count==1){
-  //   hist[1]=args[1];
-  //
-  // }else{
-  //   hist[2]=args[0];
-  //
-  // }
-  // // hist[0]=args[0];
-  // printf("%s\n", hist[(*comm_count)%10]);
-  return 1;
-}
-
-int addToHistory(int ct, char *hist[], char context[]){
-  if(ct<10){
-    for(int i=ct;i>=0;i--){
-      hist[i+1]=hist[i];
+  int addToHistory(int ct, char *hist[], char context[]){
+    if(ct<10){
+      for(int i=ct;i>-1;i--){
+        hist[i+1]=hist[i];
+      }
+    }else if (ct>=10){
+      for(int i=10;i>-1;i--){
+        hist[i+1]=hist[i];
+      }
     }
-  }else if (ct>=10){
-    for(int i=10;i>=0;i--){
-      hist[i+1]=hist[i];
-    }
+    hist[0]= malloc(MAX_LINE * sizeof(char));
+    strcat(hist[0], context);
   }
-  hist[0]= malloc(MAX_LINE * sizeof(char));
-  strcat(hist[0], context);
-}
